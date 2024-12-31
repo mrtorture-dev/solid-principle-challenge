c703f802eef39db26f94b3d46323b6d02aaa8017
Fredy Alberto Polar Urviola <91451377+mrtorture-dev@users.noreply.github.com>
2024-12-31 01:28:32 -0500

    Update D-solid-solution.py

--- solid/D-solid-solution.py
+++ solid/D-solid-solution.py
@@ -1,1 +1,16 @@
 # Here your solution
+from abc import ABC, abstractmethod
+class PaymentInterface(ABC):
+    @abstractmethod
+    def pay(amount: float):
+        pass
+class PayPalService(PaymentInterface):
+    @staticmethod
+    def pay(amount: float):
+        print(f"Paying {amount} using PayPal...")
+
+class PaymentProcessor:
+    def __init__(self,payment_service:PaymentInterface):
+        self.payment_service = payment_service
+    def process_payment(self, amount: float):
+        self.payment_service.pay(amount)
